9bd9a6f7fd7b6e76b08fc6f8b824954089d2e109
Darren Wihandi <nerradfour@gmail.com>
2021-09-05 00:13:24 -0400

    fixed bug for min_released in query builder

--- database/queries/vndb.queries.js
+++ database/queries/vndb.queries.js
@@ -158,6 +158,7 @@ const searchVn = async (params) => {
   const { query, queryParams } = advancedVnSearchQuery(params);
 
   console.log(query);
+  console.log(queryParams);
 
   try {
     const results = await db.query(query, queryParams);
@@ -180,3 +181,139 @@ export default {
   getVnReleases,
   searchVn,
 };
+
+const queryfull = `
+WITH cte (vid, min_released, max_released) AS
+(
+SELECT v.id, MIN(r.released), MAX(r.released)
+FROM vndb.releases_vn rv
+JOIN vndb.releases r ON r.id = rv.id
+JOIN vndb.releases_lang rl ON rl.id = r.id
+JOIN vndb.vn v ON v.id = rv.vid
+--JOIN releases_producers rp ON rp.id = r.id
+WHERE
+    r.released > 0 --"Released after"
+    AND r.released <= to_char(CURRENT_TIMESTAMP AT TIME ZONE 'UTC', 'YYYYMMDD')::INTEGER --"Released before". As is, checks if it's "Already released".
+    --AND (v.title ~* 'Kyonyuu' OR r.title ~* 'Kyonyuu') --String to search in VN/Release title. No normalization is done. Use = instead of ~* if you want to make an exact matching. For case sensitive match, use ~
+    --AND v.length = '3' --0 -> Unknown, 1 -> Less than 2 hours, 2 -> 2-10 hours, 3 -> 10-30 hours, 4 -> 30-50 hours, 5 ->More than 50 hours
+    AND rl.lang = 'en' --Language filter.
+    AND r.type = 'complete' --Release type. Can be also 'partial' or 'trial'.
+    AND (r.minage = '18' OR r.minage = '-1') --Age rating. -1 stands for unknown.
+    --AND r.uncensored = TRUE
+    --AND r.patch = FALSE --TRUE for patch, FALSE for standalone.
+    --AND r.freeware = FALSE --TRUE for freeware, FALSE for non-freeware.
+    --AND r.doujin = FALSE --TRUE for doujin, FALSE for commercial.
+    --AND r.voiced != '1' --Voice filter. 0 -> Unknown, 1 -> Not voiced, 2 -> Only ero scenes are voiced, 3 -> Partially voiced, 4->Fully voiced
+    --AND r.resolution = '1920x1080' 
+    --AND pid != '3392' AND pid != '6286' --Producer filter. As is, excludes companies known for MTLing VNs. To use this, you will have to uncomment "JOIN releases_producers rp ON rp.id = r.id" above.
+    --AND r.engine = 'KiriKiri' --Engine filter.
+  AND v.id IN (
+  --"Include" section for trags and traits.
+    (
+      SELECT v.id
+      FROM vndb.vn v
+      
+      
+      INTERSECT
+      
+      --Includes VNs that have Fantasy and/or its child tags with the tag score of 2 or more.
+      --Change tv.tag to tag id you want.
+      --You can simply add additional tag/trait filters with the same INTERSECT SELECT FROM... structure.
+      
+      SELECT tvi.vid
+      FROM vndb.tags_vn_inherit tvi
+      WHERE (tvi.tag = 'g1986' OR tvi.tag IN (SELECT vndb.get_child_tags('g1986')))
+          AND tvi.rating >= 2
+      
+      
+      
+      INTERSECT
+      
+      --Parent only search example with additional tag score criterion.
+      SELECT v.id
+      FROM vndb.tags_vn tv
+      JOIN vndb.vn v ON v.id = tv.vid
+      WHERE tv.tag = '105'
+      GROUP BY v.id
+      HAVING avg(tv.vote) >= 2
+      
+
+      
+            INTERSECT
+            
+            --Includes VNs where a tsundere (or one of its child trait) girl is a main character. Does not have the problem mentioned in t12507.23
+            --If you don't want to include its child tags, simply remove the "OR ct.tid IN (SELECT get_child_traits(217))" part.
+      SELECT v.id
+      FROM vndb.chars_vns cv
+            JOIN vndb.chars c ON c.id = cv.id
+            JOIN vndb.vn v ON v.id = cv.vid
+            JOIN vndb.chars_traits ct ON ct.id = c.id
+      WHERE
+          cv.role = 'primary'
+          AND c.gender = 'f'
+          AND (ct.tid = 'g217' OR ct.tid IN (SELECT vndb.get_child_traits('g217')))
+      GROUP BY v.id
+      
+    )
+    
+    EXCEPT
+    --"Exclude" section.
+    (	
+      --Excludes VNs where protagonist is a homosexual male. As is, this kind of filtering cannot be done through VNDB.
+      SELECT v.id
+      FROM vndb.chars_vns cv
+            JOIN vndb.chars c ON c.id = cv.id
+            JOIN vndb.vn v ON v.id = cv.vid
+            JOIN vndb.chars_traits ct ON ct.id = c.id
+      WHERE
+          cv.role = 'main'
+          AND c.gender = 'm'
+          AND (ct.tid = '748' OR ct.tid = '2842')
+      GROUP BY v.id
+    )
+  )
+GROUP BY v.id
+)
+SELECT v.title as "Title", 'https://vndb.org/' || v.id AS "URL", MIN(r.released) AS "Released", MIN(cte.min_released) AS "Released (Oldest)", MAX(cte.max_released) AS "Released (Latest)", c_rating || ' (' || c_votecount || ')' as "Rating",
+    CASE
+        WHEN v.length = 0 THEN 'Unknown'
+        WHEN v.length = 1 THEN '< 2 hours'
+        WHEN v.length = 2 THEN '2 - 10 hours'
+        WHEN v.length = 3 THEN '10 - 30 hours'
+        WHEN v.length = 4 THEN '30 - 50 hours'
+        ELSE '> 50 hours'
+    END AS "Length"
+FROM vndb.releases_vn rv
+JOIN vndb.releases r ON r.id = rv.id
+JOIN vndb.releases_lang rl ON rl.id = r.id
+JOIN vndb.vn v ON v.id = rv.vid
+JOIN cte ON cte.vid = v.id
+
+WHERE
+    --Original language.
+    --If you don't want to filter with original language, then simply comment this section.
+    ---- By yorhel <https://vndb.org/t12800.4>
+    v.id IN(
+        SELECT rv.vid
+        FROM vndb.releases_lang rl
+        JOIN vndb.releases_vn rv ON rv.id = rl.id
+        JOIN vndb.releases r ON r.id = rl.id
+        WHERE lang = 'ja' -- The original language you want
+            AND NOT EXISTS(
+                SELECT 1 FROM vndb.releases_vn irv JOIN vndb.releases ir ON ir.id = irv.id
+                WHERE irv.vid = rv.vid AND ir.released < r.released)
+                )
+    ----
+
+    --AND cte.max_released > '20190000'
+    --AND cte.min_released > '20190000'
+    --AND r.voiced != '1' --Checks if any release is not 'not voiced'.
+    --AND r.resolution = '1920x1080' --Checks if any realease has the given resolution
+    --AND r.freeware = FALSE --Checks if any release is non-freeware.
+    --AND r.doujin = FALSE --Checks if any release is commercial.
+
+GROUP BY v.id
+ORDER BY MAX(cte.max_released) DESC, c_rating DESC NULLS LAST --Sort by Released (Latest)
+--ORDER BY MIN(cte.min_released) DESC, c_rating DESC NULLS LAST --Sort by Released (Oldest)
+--ORDER BY MIN(r.released) DESC, c_rating DESC NULLS LAST --Sort by Released
+--ORDER BY c_rating DESC NULLS LAST, c_votecount --Sort by Rating`;
--- database/queries/vndb.query.builder.mjs
+++ database/queries/vndb.query.builder.mjs
@@ -1,3 +1,5 @@
+import { concat } from "lodash";
+
 /*
   SEARCH VN 
   
@@ -57,14 +59,11 @@ export const advancedVnSearchQuery = (params) => {
 
   if (params.search !== undefined) {
     query = query.concat(`
-    AND (v.title ~* $${currIndex} OR r.title ~* $${currIndex + 1} OR 
-      v.alias ~* $${currIndex + 2} OR v.original ~* $${currIndex + 3})
+    AND (v.title ~* $${currIndex} OR r.title ~* $${currIndex} OR 
+      v.alias ~* $${currIndex} OR v.original ~* $${currIndex})
     `);
     queryParams.push(params.search);
-    queryParams.push(params.search);
-    queryParams.push(params.search);
-    queryParams.push(params.search);
-    currIndex += 4;
+    currIndex++;
   }
 
   if (params.tags !== undefined) {
@@ -78,14 +77,21 @@ export const advancedVnSearchQuery = (params) => {
     // parent only for now
     params.tags.forEach((tagid) => {
       query = query.concat(`
-        INTERSECT
-        SELECT v.id
-        FROM vndb.tags_vn tv
-        JOIN vndb.vn v ON v.id = tv.vid
-        WHERE tv.tag = $${currIndex}
-        GROUP BY v.id
-        HAVING avg(tv.vote) >= 2
+      INTERSECT
+      SELECT tvi.vid
+      FROM vndb.tags_vn_inherit tvi
+      WHERE (tvi.tag = $${currIndex} OR tvi.tag IN (SELECT vndb.get_child_tags($${currIndex})))
+          AND tvi.rating >= 2
       `);
+      // query = query.concat(`
+      //   INTERSECT
+      //   SELECT v.id
+      //   FROM vndb.tags_vn tv
+      //   JOIN vndb.vn v ON v.id = tv.vid
+      //   WHERE tv.tag = $${currIndex}
+      //   GROUP BY v.id
+      //   HAVING avg(tv.vote) >= 2
+      // `);
       currIndex++;
       queryParams.push(tagid);
     });
@@ -151,14 +157,16 @@ export const advancedVnSearchQuery = (params) => {
       cursor = cursor.concat(" c_popularity ");
     } else if (sort_by === "rating") {
       cursor = cursor.concat(" c_rating ");
+    } else if (params.sort_by === "min_released") {
+      cursor = cursor.concat(" cte.min_released ");
     } else if (sort_by === "max_released") {
       cursor = cursor.concat(" cte.max_released ");
     } else {
-      cursor = cursor.concat(" cte.max_released "); // default max_released
+      cursor = cursor.concat(" popularity ");
     }
 
     let operator = "";
-    if (params.sort_order === "acsending") {
+    if (params.sort_order === "ascending") {
       operator = operator.concat(` > `);
     } else {
       operator = operator.concat(` < `);
@@ -191,10 +199,14 @@ export const advancedVnSearchQuery = (params) => {
       query = query.concat(" c_popularity ");
     } else if (params.sort_by === "rating") {
       query = query.concat(" c_rating ");
+    } else if (params.sort_by === "released") {
+      query = query.concat(" released ");
     } else if (params.sort_by === "max_released") {
-      query = query.concat(" MAX(cte.max_released) ");
+      query = query.concat(" max_released ");
+    } else if (params.sort_by === "min_released") {
+      query = query.concat(" min_released ");
     } else {
-      query = query.concat(" MAX(cte.max_released) ");
+      query = query.concat(" c_popularity ");
     }
 
     if (params.sort_order !== undefined) {
@@ -232,139 +244,3 @@ export const advancedVnSearchQuery = (params) => {
     queryParams: queryParams,
   };
 };
-
-const queryfull = `
-  WITH cte (vid, min_released, max_released) AS
-  (
-  SELECT v.id, MIN(r.released), MAX(r.released)
-  FROM vndb.releases_vn rv
-  JOIN vndb.releases r ON r.id = rv.id
-  JOIN vndb.releases_lang rl ON rl.id = r.id
-  JOIN vndb.vn v ON v.id = rv.vid
-  --JOIN releases_producers rp ON rp.id = r.id
-  WHERE
-      r.released > 0 --"Released after"
-      AND r.released <= to_char(CURRENT_TIMESTAMP AT TIME ZONE 'UTC', 'YYYYMMDD')::INTEGER --"Released before". As is, checks if it's "Already released".
-      --AND (v.title ~* 'Kyonyuu' OR r.title ~* 'Kyonyuu') --String to search in VN/Release title. No normalization is done. Use = instead of ~* if you want to make an exact matching. For case sensitive match, use ~
-      --AND v.length = '3' --0 -> Unknown, 1 -> Less than 2 hours, 2 -> 2-10 hours, 3 -> 10-30 hours, 4 -> 30-50 hours, 5 ->More than 50 hours
-      AND rl.lang = 'en' --Language filter.
-      AND r.type = 'complete' --Release type. Can be also 'partial' or 'trial'.
-      AND (r.minage = '18' OR r.minage = '-1') --Age rating. -1 stands for unknown.
-      --AND r.uncensored = TRUE
-      --AND r.patch = FALSE --TRUE for patch, FALSE for standalone.
-      --AND r.freeware = FALSE --TRUE for freeware, FALSE for non-freeware.
-      --AND r.doujin = FALSE --TRUE for doujin, FALSE for commercial.
-      --AND r.voiced != '1' --Voice filter. 0 -> Unknown, 1 -> Not voiced, 2 -> Only ero scenes are voiced, 3 -> Partially voiced, 4->Fully voiced
-      --AND r.resolution = '1920x1080' 
-      --AND pid != '3392' AND pid != '6286' --Producer filter. As is, excludes companies known for MTLing VNs. To use this, you will have to uncomment "JOIN releases_producers rp ON rp.id = r.id" above.
-      --AND r.engine = 'KiriKiri' --Engine filter.
-    AND v.id IN (
-    --"Include" section for trags and traits.
-      (
-        SELECT v.id
-        FROM vndb.vn v
-        
-        
-        INTERSECT
-        
-        --Includes VNs that have Fantasy and/or its child tags with the tag score of 2 or more.
-        --Change tv.tag to tag id you want.
-        --You can simply add additional tag/trait filters with the same INTERSECT SELECT FROM... structure.
-        
-        SELECT tvi.vid
-        FROM vndb.tags_vn_inherit tvi
-        WHERE (tvi.tag = 'g1986' OR tvi.tag IN (SELECT vndb.get_child_tags('g1986')))
-            AND tvi.rating >= 2
-        
-        
-        
-        INTERSECT
-        
-        --Parent only search example with additional tag score criterion.
-        SELECT v.id
-        FROM vndb.tags_vn tv
-        JOIN vndb.vn v ON v.id = tv.vid
-        WHERE tv.tag = '105'
-        GROUP BY v.id
-        HAVING avg(tv.vote) >= 2
-        
-  
-        
-              INTERSECT
-              
-              --Includes VNs where a tsundere (or one of its child trait) girl is a main character. Does not have the problem mentioned in t12507.23
-              --If you don't want to include its child tags, simply remove the "OR ct.tid IN (SELECT get_child_traits(217))" part.
-        SELECT v.id
-        FROM vndb.chars_vns cv
-              JOIN vndb.chars c ON c.id = cv.id
-              JOIN vndb.vn v ON v.id = cv.vid
-              JOIN vndb.chars_traits ct ON ct.id = c.id
-        WHERE
-            cv.role = 'primary'
-            AND c.gender = 'f'
-            AND (ct.tid = 'g217' OR ct.tid IN (SELECT vndb.get_child_traits('g217')))
-        GROUP BY v.id
-        
-      )
-      
-      EXCEPT
-      --"Exclude" section.
-      (	
-        --Excludes VNs where protagonist is a homosexual male. As is, this kind of filtering cannot be done through VNDB.
-        SELECT v.id
-        FROM vndb.chars_vns cv
-              JOIN vndb.chars c ON c.id = cv.id
-              JOIN vndb.vn v ON v.id = cv.vid
-              JOIN vndb.chars_traits ct ON ct.id = c.id
-        WHERE
-            cv.role = 'main'
-            AND c.gender = 'm'
-            AND (ct.tid = '748' OR ct.tid = '2842')
-        GROUP BY v.id
-      )
-    )
-  GROUP BY v.id
-  )
-  SELECT v.title as "Title", 'https://vndb.org/' || v.id AS "URL", MIN(r.released) AS "Released", MIN(cte.min_released) AS "Released (Oldest)", MAX(cte.max_released) AS "Released (Latest)", c_rating || ' (' || c_votecount || ')' as "Rating",
-      CASE
-          WHEN v.length = 0 THEN 'Unknown'
-          WHEN v.length = 1 THEN '< 2 hours'
-          WHEN v.length = 2 THEN '2 - 10 hours'
-          WHEN v.length = 3 THEN '10 - 30 hours'
-          WHEN v.length = 4 THEN '30 - 50 hours'
-          ELSE '> 50 hours'
-      END AS "Length"
-  FROM vndb.releases_vn rv
-  JOIN vndb.releases r ON r.id = rv.id
-  JOIN vndb.releases_lang rl ON rl.id = r.id
-  JOIN vndb.vn v ON v.id = rv.vid
-  JOIN cte ON cte.vid = v.id
-  
-  WHERE
-      --Original language.
-      --If you don't want to filter with original language, then simply comment this section.
-      ---- By yorhel <https://vndb.org/t12800.4>
-      v.id IN(
-          SELECT rv.vid
-          FROM vndb.releases_lang rl
-          JOIN vndb.releases_vn rv ON rv.id = rl.id
-          JOIN vndb.releases r ON r.id = rl.id
-          WHERE lang = 'ja' -- The original language you want
-              AND NOT EXISTS(
-                  SELECT 1 FROM vndb.releases_vn irv JOIN vndb.releases ir ON ir.id = irv.id
-                  WHERE irv.vid = rv.vid AND ir.released < r.released)
-                  )
-      ----
-  
-      --AND cte.max_released > '20190000'
-      --AND cte.min_released > '20190000'
-      --AND r.voiced != '1' --Checks if any release is not 'not voiced'.
-      --AND r.resolution = '1920x1080' --Checks if any realease has the given resolution
-      --AND r.freeware = FALSE --Checks if any release is non-freeware.
-      --AND r.doujin = FALSE --Checks if any release is commercial.
-  
-  GROUP BY v.id
-  ORDER BY MAX(cte.max_released) DESC, c_rating DESC NULLS LAST --Sort by Released (Latest)
-  --ORDER BY MIN(cte.min_released) DESC, c_rating DESC NULLS LAST --Sort by Released (Oldest)
-  --ORDER BY MIN(r.released) DESC, c_rating DESC NULLS LAST --Sort by Released
-  --ORDER BY c_rating DESC NULLS LAST, c_votecount --Sort by Rating`;
